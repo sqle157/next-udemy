de6c85a294495f6d7fa272ba7caa50df401d3c84
Sang Le <sqle157@gmail.com>
2024-02-27 22:00:11 +0700

    Adding more basic setups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
-import Link from 'next/link';
+import Image from 'next/image';
+import homeImg from 'public/home.jpg';
 
 export default function Home() {
 	return (
 		<div>
-			<div>
-				<Link href='/performance'>Performance</Link>
-				<Link href='/reliability'>Reliability</Link>
-				<Link href='/scale'>Scale</Link>
-			</div>
 			Home Page
+			<div className='absolute -z-10 inset-0'>
+				<Image
+					src={homeImg}
+					alt='car factory'
+					fill
+					style={{ objectFit: 'cover' }}
+				/>
+			</div>
 		</div>
 	);
 }
